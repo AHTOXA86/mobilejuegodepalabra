9cab23d588bb86fd142ce9ba8a6bac27fbe8a2a2
Anton Chahan <chaganton@gmail.com>
2024-09-04 11:47:21 +0200

    Add event for start game.

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {IonButton, IonInput, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
-  imports: [IonButton, IonInput, IonGrid, IonRow, IonCol],
+  imports: [FormsModule, IonButton, IonInput, IonGrid, IonRow, IonCol],
   standalone: true,
 })
 export class SignInComponent  implements OnInit {
+  @Output('playerName') changeStateOfGame = new EventEmitter<string>();
 
+  playerName: string = '';
   constructor() { }
 
   ngOnInit() {}
 
   startGame() {
-    console.log('start!!!');
-    
+    console.log(`Starting game for player: ${this.playerName}`);
+    this.changeStateOfGame.emit(this.playerName)
   }
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 // import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { SignInComponent } from '../sign-in/sign-in.component';
+import { GameComponent } from '../game/game.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, SignInComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, SignInComponent, GameComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() { }
 
   isGame = false
   name = ''
 
-  
+  setGameState($event: any) {
+    this.isGame = true; 
+    console.log($event);
+    this.name = $event.data;
+  }
+
 }
